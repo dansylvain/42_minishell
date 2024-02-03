6b3c75334070ea7ccc77bc287c17245ba764f6eb
dansylvain <dansylvain@gmail.com>
2024-02-03 19:28:17 +0100

    added script to compare minishell and bash

---
+++ compare_bash_and_minishell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Définir le chemin de votre exécutable Minishell
+MINISHELL_EXECUTABLE="./minishell"
+
+# Liste des commandes à tester
+commands=(
+    "ls"
+    "pwd"
+    "echo Hello, World!"
+    # Ajoutez d'autres commandes ici
+)
+
+# Fonction pour comparer les sorties
+compare_outputs() {
+    command_output=$($1)
+    minishell_output=$(echo "$1" | $MINISHELL_EXECUTABLE)
+
+    if [ "$command_output" != "$minishell_output" ]; then
+        echo "Différence détectée pour la commande: $1"
+        echo "Sortie Bash: $command_output"
+        echo "Sortie Minishell: $minishell_output"
+        echo "---------------------------------------"
+    fi
+}
+
+# Exécutez les commandes et comparez les sorties
+for cmd in "${commands[@]}"; do
+    compare_outputs "$cmd"
+done
+
+echo "Comparaison terminée."
