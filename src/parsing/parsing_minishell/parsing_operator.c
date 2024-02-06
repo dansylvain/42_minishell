c547e1918b46048f76c10e09eba81236ac872cac
sierra182 <sierra-echo@msn.com>
2024-02-06 19:07:58 +0100

    debug set op

--- src/parsing/parsing_minishell/parsing_operator.c
+++ src/parsing/parsing_minishell/parsing_operator.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 10:29:44 by svidot            #+#    #+#             */
-/*   Updated: 2024/02/06 18:04:24 by seblin           ###   ########.fr       */
+/*   Updated: 2024/02/06 18:57:28 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,23 +147,20 @@ t_ast_nde	*set_operator(t_ast_nde *node)
 	t_ast_nde *sib_parent;
 	t_ast_nde *token;
 	t_ast_nde *raw_lft;
-	
+	t_ast_nde *raw_rght;
+	printf("une fois\n");
 	sib_parent = node->child;
 	sib = sib_parent->child;
 	token = create_token_node(sib);
 	sib_parent->sibling = token;
-	// printf("test1\n");
-	// if (sib)
-	// 	printf("test2\n");
-	// else 
-	// 	printf("test3\n");
 	if (token)
 	{
 		raw_lft = create_token_child(sib_parent, token);
 		token->child = raw_lft;
-		// if (sib)
-		// 	printf("test42\n");
-		fill_child(sib, raw_lft, raw_lft->sibling, token);	
+		raw_rght = raw_lft->sibling;
+		raw_rght->child = copy_node(raw_rght);
+		fill_child(sib, raw_lft, raw_rght, token);
+		set_operator(raw_lft->sibling);
 	}
 	
 	// if (pipe)
