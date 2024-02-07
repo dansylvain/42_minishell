c38d4b461e65d131c34cb64e7ab9b0a7fc5595aa
dansylvain <dansylvain@gmail.com>
2024-02-07 14:49:32 +0100

    start working on formating ast to char tab

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 14:04:56 by dan               #+#    #+#             */
-/*   Updated: 2024/02/07 13:43:25 by seblin           ###   ########.fr       */
+/*   Updated: 2024/02/07 14:41:51 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,15 +103,14 @@ char	**create_command_tab(t_ast_nde *node)
 	if (commands_tab == 0)
 		return (NULL);
 	i = 0;
-	// while (i < tree_length)
-	// {
-	// 	commands_tab[i] = ft_strndup(node->start, node->end - node->start + 1);
-	// 	node = node->sibling;
-	// 	ft_printf("commands_tab[%i]: %s\n", i,  commands_tab[i]);
-	// 	if (commands_tab[i] == NULL)
-	// 		return (NULL);
-	// 	i++;
-	// }
+	while (i < tree_length)
+	{
+		commands_tab[i] = ft_strndup(node->start, node->end - node->start + 1);
+		node = node->sibling;
+		if (commands_tab[i] == NULL)
+			return (NULL);
+		i++;
+	}
 	commands_tab[i] = NULL;
 	return (commands_tab);
 }
@@ -121,10 +120,10 @@ void	display_command_tab(char **command_tab)
 	int i;
 
 	i = 0;
-	// ft_printf("command_tab: \n");
+	ft_printf("command_tab: \n");
 	while (command_tab[i])
 	{
-		ft_printf("%s\n", command_tab[i]);
+		ft_printf("commands_tab[%i]: %s\n", i,  command_tab[i]);
 		i++;
 	}
 }
@@ -138,7 +137,7 @@ int	command_is_builtin(char	*cmd[], t_Data *data, char *envp[])
 {
 	char	**cmd_tab;
 
-	 parse(cmd[0]); exit(1);
+	//  parse(cmd[0]); exit(1);
 	cmd_tab = create_command_tab(parse(cmd[0]));	
 	display_command_tab(cmd_tab);
 	pipex(cmd_tab, envp);
