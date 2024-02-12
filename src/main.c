f07829fe91fe08e62131d842a0d4472d81fbadbe
dansylvain <dansylvain@gmail.com>
2024-02-12 12:15:39 +0100

    added path to prompt

--- src/main.c
+++ src/main.c
@@ -1,43 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/17 14:04:56 by dan               #+#    #+#             */
-/*   Updated: 2024/02/11 12:48:11 by dan              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+	/* ************************************************************************** */
+	/*                                                                            */
+	/*                                                        :::      ::::::::   */
+	/*   main.c                                             :+:      :+:    :+:   */
+	/*                                                    +:+ +:+         +:+     */
+	/*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
+	/*                                                +#+#+#+#+#+   +#+           */
+	/*   Created: 2024/01/17 14:04:56 by dan               #+#    #+#             */
+	/*   Updated: 2024/02/12 11:58:29 by dan              ###   ########.fr       */
+	/*                                                                            */
+	/* ************************************************************************** */
 
-#include "../includes/minishell.h"
-#include "parsing_utils.h"
+	#include "../includes/minishell.h"
+	#include "parsing_utils.h"
 
-char		**parse_cmd(char **command, char **env);
-void		tmp_main(void);
-void		display_command_tab(char **command_tab);
-int			pipex(char *argv[], char *envp[]);
-t_ast_nde	*parse(char *str);
-char		**create_command_tab(t_Data *data, t_ast_nde *node, char *envp[]);
+	char		**parse_cmd(char **command, char **env);
+	void		tmp_main(void);
+	void		display_command_tab(char **command_tab);
+	int			pipex(char *argv[], char *envp[]);
+	t_ast_nde	*parse(char *str);
+	char		**create_command_tab(t_Data *data, t_ast_nde *node, char *envp[]);
 
-/**========================================================================
- *                             COMMENTS POLICY
- *? signal info at the end of a function's comment block 
- *? signal info in the main comment bloc
- *? add your name at the beggining of a comment
- *---
- *TODO finish builtin export
- *TODO fix memory leaks export
- *TODO fix memory leaks unset
- *========================================================================**/
+	/**========================================================================
+	 *                             COMMENTS POLICY
+	 *? signal info at the end of a function's comment block 
+	*? signal info in the main comment bloc
+	*? add your name at the beggining of a comment
+	*---
+	*TODO finish builtin export
+	*TODO fix memory leaks export
+	*TODO fix memory leaks unset
+	*========================================================================**/
 
-/**========================================================================
- *                           main.c
- *? rl_catch_signals is a global variable (from readline lib)
- *? it is used to ignore SIGQUILL (see handle_signals.c)
-*========================================================================**/
-int	main(int argc, char **argv, char *envp[])
-{
+	/**========================================================================
+	 *                           main.c
+	 *? rl_catch_signals is a global variable (from readline lib)
+	*? it is used to ignore SIGQUILL (see handle_signals.c)
+	*========================================================================**/
+	int	main(int argc, char **argv, char *envp[])
+	{
 	t_Data	*data;
 
 	rl_catch_signals = 0;
@@ -53,24 +53,46 @@ int	main(int argc, char **argv, char *envp[])
 	if (prompt_loop(data, envp) == 0)
 		return (free_data(data), 0);
 	return (0);
-}
+	}
 
-/**========================================================================
- *                           
- * possibility to add the path before prompt with this function
- * (write a function "build_prompt")
- * prompt = getcwd(NULL, 0);
- *========================================================================**/
-int	prompt_loop(t_Data *data, char *envp[])
-{
+	void build_prompt(char prompt[]) {
+	char cwd[1024];
+	char *home;
+
+	home = getenv("HOME");
+	ft_strlcpy(prompt, "minishell: ", 12);
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		char *shortened_cwd = strstr(cwd, home);
+		if (shortened_cwd != NULL)
+			strcat(prompt, shortened_cwd + strlen(home));
+		else
+			strcat(prompt, cwd);
+	}
+	else
+		perror("getcwd");
+	strcat(prompt, "$ ");
+	}
+
+
+	/**========================================================================
+	 *                           
+	 * possibility to add the path before prompt with this function
+	 * (write a function "build_prompt")
+	 * prompt = getcwd(NULL, 0);
+	 *========================================================================**/
+	int	prompt_loop(t_Data *data, char *envp[])
+	{
 	char	*cmd[2];
-	char	*prompt;
+	char	prompt[1024];
 	char	***cmd_tab;
 
+	ft_bzero(prompt, 1024);
+	build_prompt(prompt);
 	cmd[1] = NULL;
 	while (1)
 	{
-		cmd[0] = readline("minishell> ");
+		cmd[0] = readline(prompt);
 		if (cmd[0] && *cmd[0])
 			add_history(cmd[0]);
 		if (cmd[0] == NULL)
@@ -79,15 +101,15 @@ int	prompt_loop(t_Data *data, char *envp[])
 		free(cmd[0]);
 	}
 	return (1);
-}
+	}
 
-/**========================================================================
- *                           command_is_builtin
- * exit builtin implemented without extern function
- * 0 is returned, and the data struct is freed in calling function
- *========================================================================**/
-int	command_is_builtin(char	*cmd_tab[], t_Data *data, char *envp[])
-{
+	/**========================================================================
+	 *                           command_is_builtin
+	 * exit builtin implemented without extern function
+	 * 0 is returned, and the data struct is freed in calling function
+	 *========================================================================**/
+	int	command_is_builtin(char	*cmd_tab[], t_Data *data, char *envp[])
+	{
 	int		return_pipex;
 
 	if (!cmd_tab)
@@ -114,13 +136,13 @@ int	command_is_builtin(char	*cmd_tab[], t_Data *data, char *envp[])
 	// 	return (ft_printf("exit\n"), free_command_tab(cmd_tab), 0);
 	//free_command_tab(cmd_tab);
 	return (0);
-}
+	}
 
-/**========================================================================
- *                           duplicate_envp
- *========================================================================**/
-char	**duplicate_envp(t_Data *data, char *envp[])
-{
+	/**========================================================================
+	 *                           duplicate_envp
+	 *========================================================================**/
+	char	**duplicate_envp(t_Data *data, char *envp[])
+	{
 	char	**envp_tab;
 	int		i;
 
@@ -141,4 +163,4 @@ char	**duplicate_envp(t_Data *data, char *envp[])
 	}
 	envp_tab[i] = NULL;
 	return (envp_tab);
-}
+	}
