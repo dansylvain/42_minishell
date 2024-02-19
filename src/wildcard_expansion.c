5b5f96a343fe252c7d70d830f778e2e618471107
dansylvain <dansylvain@gmail.com>
2024-02-19 12:28:02 +0100

    norminating wildcardexpansionfile

--- src/wildcard_expansion.c
+++ src/wildcard_expansion.c
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 10:14:43 by dan               #+#    #+#             */
-/*   Updated: 2024/02/18 11:40:05 by dan              ###   ########.fr       */
+/*   Updated: 2024/02/19 12:27:11 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-int matches_pattern(const char *filename, const char *pattern);
+int	matches_pattern(const char *filename, const char *pattern);
 
-char **expand_wildcards(const char *pattern)
+char	**expand_wildcards(const char *pattern)
 {
 	DIR *dir;
 	struct dirent *entry;
 	char **matches = NULL;
 	size_t count = 0;
 
-	if ((dir = opendir(".")) != NULL)
+	dir = opendir(".");
+	if ((dir) != NULL)
 	{
 		while ((entry = readdir(dir)) != NULL)
-	{
+		{
 			if (matches_pattern(entry->d_name, pattern))
 			{
 				matches = ft_realloc(matches, (count + 1) * sizeof(char *));
@@ -55,28 +56,34 @@ char **expand_wildcards(const char *pattern)
 		exit(EXIT_FAILURE);
 	}
 	matches[count] = NULL;
-	return matches;
+	return (matches);
 }
 
-
-
-int matches_pattern(const char *filename, const char *pattern)
+int	matches_pattern(const char *filename, const char *pattern)
 {
-	size_t filename_len = ft_strlen(filename);
-	size_t pattern_len = ft_strlen(pattern);
-	size_t i = 0, j = 0;
+	size_t	filename_len;
+	size_t	pattern_len;
+	size_t	i;
+	size_t	j;
 
+	filename_len = ft_strlen(filename);
+	pattern_len = ft_strlen(pattern);
+	i = 0;
+	j = 0;
 	while (i < filename_len && j < pattern_len)
 	{
 		if (pattern[j] == '*')
 		{
-			if (pattern[j + 1] == '\0') return 1;
-			while (i < filename_len && filename[i] != pattern[j + 1]) i++;
+			if (pattern[j + 1] == '\0')
+				return (1);
+			while (i < filename_len && filename[i] != pattern[j + 1])
+				i++;
 			j++;
 		}
 		else
 		{
-			if (filename[i] != pattern[j]) return 0;
+			if (filename[i] != pattern[j])
+				return (0);
 			i++;
 			j++;
 		}
@@ -89,19 +96,23 @@ int	has_wildcard(char *str)
 	return (ft_strchr(str, '*') != NULL);
 }
 
-int wilcard_func(char *pattern)
+int	wilcard_func(char *pattern)
 {
-    char **matches;
+	size_t	i;
+	char	**matches;
+
 	if (!has_wildcard(pattern))
-        return EXIT_SUCCESS;
-    matches = expand_wildcards(pattern);
-    if (matches[0] != NULL)
+		return (EXIT_SUCCESS);
+	matches = expand_wildcards(pattern);
+	if (matches[0] != NULL)
 	{
-        for (size_t i = 0; matches[i] != NULL; i++)
+		i = 0;
+		while (matches[i] != NULL)
 		{
-            ft_printf("%s\n", matches[i]);
-            free(matches[i]);
-        }
-    }
-    free(matches);
+			ft_printf("%s\n", matches[i]);
+			free(matches[i]);
+			i++;
+		}
+	}
+	free (matches);
 }
