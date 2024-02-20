dae238622e806c56ec6d23c058e7e0f815ba443b
dansylvain <dansylvain@gmail.com>
2024-02-20 09:47:18 +0100

    pouet

--- src/builtins/exec_export.c
+++ src/builtins/exec_export.c
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 09:08:19 by dan               #+#    #+#             */
-/*   Updated: 2024/02/20 09:12:37 by dan              ###   ########.fr       */
+/*   Updated: 2024/02/20 09:45:06 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	exec_export(char **command_tab, t_Data *data)
 	{
 		if (!are_only_valid_chars(command_tab[i]))
 		{
-			display_error_detail("minishell: export: ", command_tab[i], ": not a valid identifier\n");
+			display_error_detail("minishell: export: `", command_tab[i], "': not a valid identifier\n");
 			data->exit_status = 1;
 			return (0);
 		}
