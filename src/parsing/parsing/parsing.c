ea2ed680f54fb3740711f796ad0dda4f21964105
dansylvain <dansylvain@gmail.com>
2024-02-26 09:39:13 +0100

    norm parsing

--- src/parsing/parsing/parsing.c
+++ src/parsing/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 15:18:58 by seblin            #+#    #+#             */
-/*   Updated: 2024/02/25 16:59:22 by seblin           ###   ########.fr       */
+/*   Updated: 2024/02/26 09:38:57 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 #include "parsing_utils.h"
 #include "libft.h"
 #include "minishell.h"
-//#include <stdio.h>
-//#include "ft_printf.h"
 
 t_ast_nde	*copy_node(t_ast_nde *node);
 t_ast_nde	*set_qute_sib(char *str);
 t_ast_nde	*set_operator(t_ast_nde *node);
-void		leaf_tree(t_ast_nde *operator, t_ast_nde **rslt, t_ast_nde **rslt_sav, t_Data *data);
+void		leaf_tree(t_ast_nde *operator, t_ast_nde **rslt,
+				t_ast_nde **rslt_sav, t_Data *data);
 void		free_tree(t_ast_nde *operator);
 void		print_qute_sib(t_ast_nde *sib);
 void		print_tree(t_ast_nde *node);
 void		print_cmd(t_ast_nde *cmd);
-	
+
 t_ast_nde	*parse(char *str, t_Data *data)
 {
 	t_ast_nde	*root;
 	t_ast_nde	*cmd_sav;
 	t_ast_nde	*cmd;
-	
+
 	if (!*str)
 		return (NULL);
 	cmd_sav = NULL;
@@ -41,14 +40,8 @@ t_ast_nde	*parse(char *str, t_Data *data)
 	root->child = copy_node(root);
 	root->child->child = copy_node(root);
 	root->child->child->child = set_qute_sib(str);
-	//print_qute_sib(root->child->child->child);	
 	set_operator(root->child);
 	leaf_tree(root, &cmd, &cmd_sav, data);
-	//print_tree(root->child->child->sibling);
-	//print_cmd(cmd_sav);
-	
 	free_tree(root);
 	return (cmd_sav);
 }
-
-
