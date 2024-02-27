7bf73874eee6a961f9ecd3146bf4f529192f0904
sierra182 <sierra-echo@msn.com>
2024-02-27 22:22:14 +0100

    refact nurcery

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 15:23:23 by svidot            #+#    #+#             */
-/*   Updated: 2024/02/27 19:04:21 by seblin           ###   ########.fr       */
+/*   Updated: 2024/02/27 22:19:02 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@
 #include "../../includes/minishell.h"
 
 void	close_fd(int fd[]);
+void	close_fds(int fd[]);
 // void	set_filepath_and_delim(int *argc, char **argv[], t_redir *redir);
 int	get_fdio(t_redir *redir);
 //char	**parse_cmd(char *argv[], char *envp[]);
@@ -38,12 +39,10 @@ void	set_pipe_forward(int pipefd_in[], int pipefd_out[], t_redir redir)
 	if (redir.redir[0])
 	{		
 		dup2(pipefd_in[0], STDIN_FILENO);
-		close(pipefd_in[0]);
-		close(pipefd_in[1]);
+		close_fds(pipefd_in);		
 	}		
 	dup2(pipefd_out[1], STDOUT_FILENO);
-	close(pipefd_out[1]);
-	close(pipefd_out[0]);
+	close_fds(pipefd_out);	
 }
 
 void	here_doc_handle(int pipefd_in[], t_redir redir) //argv!!
@@ -78,6 +77,7 @@ void	here_doc_handle(int pipefd_in[], t_redir redir) //argv!!
 	}
 }
 
+
 int set_redir_out(char **argv, t_redir *redir)
 {
 	if (**argv == '>')
@@ -104,7 +104,7 @@ int	set_redir_in(char **argv, t_redir *redir)
 		{
 			redir->delim = argv[1];
 			redir->redir[0] = 2;
-			close_fd(redir->pipe);
+			close_fds(redir->pipe);
 			pipe(redir->pipe);
 			here_doc_handle(redir->pipe, *redir);		
 		}
@@ -118,18 +118,6 @@ int	set_redir_in(char **argv, t_redir *redir)
 			}
 		}			
 	}
-	// else if (**argv == '>')
-	// {
-	// 	redir->redir[1] = 1;		
-	// 	if (!ft_strcmp(*argv, ">>"))
-	// 		redir->redir[1] = 2;
-	// 	redir->filepath[1] = argv[1];
-	// 	if (get_fdio(redir))
-	// 	{
-	// 		//redir->redir[1] = 0;
-	// 		return (1);
-	// 	}
-	// }
 	return (0);
 }
 
@@ -183,46 +171,51 @@ void	init_redir( t_redir *redir)
 	redir->filepath[0] = NULL;
 	redir->filepath[1] = NULL;
 }
+void	switch_pipes(int *pipefd[])
+{	
+	close_fds(pipefd[0]);	
+	pipefd[0][0] = pipefd[1][0];
+	pipefd[0][1] = pipefd[1][1];
+}
+void	builtin_or_execve(char **argv[], char *envp[])
+{
+	if (!command_is_builtin(*argv, NULL, envp))
+	{
+		if (access(**argv, X_OK))
+			search_path(*argv, envp);
+		execve(**argv, *argv, envp);
+		exit(EXIT_FAILURE);
+	}
+	else								
+		exit(EXIT_SUCCESS);
+}
 
 pid_t	nurcery(char **argv[], char *envp[], int fd_file[], int *pipefd[], t_redir *redir)
 {
 	pid_t	pid;
-	
-	init_redir(redir);
-	if (set_all_redir_in(argv, redir))	
-		exit(1);
-	if (set_all_redir_out(argv, redir))
-		exit(1);
-	set_pipefd_in(pipefd[0], redir);
+		
+	init_redir(redir);	
 	while (*argv)
 	{						
-		pipe(pipefd[1]);		
 		if (***argv != '>' && ***argv != '<')
 		{
+			pipe(pipefd[1]);		
 			pid = fork();
-			if (pid == 0)
-			{
-				if (fd_file[1] > 2)
-					close(fd_file[1]);
+			if (!pid)
+			{							
+				close_fd(fd_file[1]);
+				
+				if (set_all_redir_in(argv, redir))	
+					exit(1);
+				if (set_all_redir_out(argv, redir))
+					exit(1);	
+				set_pipefd_in(pipefd[0], redir);						
 				set_pipe_forward(pipefd[0], pipefd[1], *redir);
-				if (!command_is_builtin(*argv, NULL, envp))
-				{
-					if (access(**argv, X_OK))
-						search_path(*argv, envp);
-					execve(**argv, *argv, envp);
-					exit(EXIT_FAILURE);
-				}
-				else								
-					exit(EXIT_SUCCESS);					
+				
+				builtin_or_execve(argv, envp);						
 			}
-			else
-			{				
-				close(pipefd[0][1]);
-				if (pipefd[0][0] > 2)
-					close(pipefd[0][0]);
-				pipefd[0][0] = pipefd[1][0];
-				pipefd[0][1] = pipefd[1][1];							
-			}
+			else						
+				switch_pipes(pipefd);
 		}
 		argv++;
 	}
@@ -236,8 +229,7 @@ void	pipe_to_screen(int pipe, t_redir redir)
 	if (redir.redir[1])
 	{
 		while (read(pipe, &buf, 1))
-			ft_putchar_fd(buf, redir.fd_file[1]);		
-		close(redir.fd_file[1]);		
+			ft_putchar_fd(buf, redir.fd_file[1]);	
 	}
 	else
 	{		
@@ -258,11 +250,10 @@ pid_t	create_pipeline(char **argv[], char *envp[], t_redir redir)
 	pipefd_out[0] = -1;
 	pipefd_out[1] = -1;
 	pid = nurcery(argv, envp, redir.fd_file, (int *[]){pipefd_in, pipefd_out}, &redir);		
-	close(pipefd_in[1]);
+	close_fd(pipefd_in[1]);
 	pipe_to_screen(pipefd_in[0], redir);
-	close(pipefd_in[0]);
-	close(pipefd_out[0]);
-	close(pipefd_out[1]);
+	close_fd(pipefd_in[0]);
+	close_fds(pipefd_out);	
 	return (pid);
 }
 
