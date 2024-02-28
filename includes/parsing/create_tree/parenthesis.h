60c1534a697736e4492978c2405fff1944fc8b12
sierra182 <sierra-echo@msn.com>
2024-02-28 16:18:32 +0100

    norme headers

--- includes/parsing/create_tree/parenthesis.h
+++ includes/parsing/create_tree/parenthesis.h
@@ -6,13 +6,11 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 14:28:02 by seblin            #+#    #+#             */
-/*   Updated: 2024/02/28 14:31:29 by seblin           ###   ########.fr       */
+/*   Updated: 2024/02/28 16:17:05 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARENTHESIS_H
 # define PARENTHESIS_H
-
-
 
 #endif
--- includes/pipex/redir.h
+++ includes/pipex/redir.h
@@ -6,16 +6,16 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 14:22:59 by seblin            #+#    #+#             */
-/*   Updated: 2024/02/28 15:22:45 by seblin           ###   ########.fr       */
+/*   Updated: 2024/02/28 16:16:46 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef REDIR_H
+# define REDIR_H
 
-#include "pipex_utils.h"
-#include <stdlib.h>
-#include "libft.h"
+# include "pipex_utils.h"
+# include <stdlib.h>
+# include "libft.h"
 
 int		get_fdio(t_redir *redir);
 void	here_doc_handle(int pipefd_in[], t_redir redir);
