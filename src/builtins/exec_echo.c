67077a22450e0fa6adad9d14f7de15346803649d
dansylvain <dansylvain@gmail.com>
2024-03-03 09:37:00 +0100

    echo bug fixed

--- src/builtins/exec_echo.c
+++ src/builtins/exec_echo.c
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 17:43:09 by dan               #+#    #+#             */
-/*   Updated: 2024/02/22 10:52:07 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/03 09:36:20 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,34 @@
 /**========================================================================
  *                           exec_echo
  *========================================================================**/
-void	exec_echo(t_Data *data, char **command_tab)
-{
-	int	i;
-	int	j;
+	void	exec_echo(t_Data *data, char **command_tab)
+	{
+		int	i;
+		int	j;
 
-	i = 1;
-	if (!command_tab[1] || is_only_space(command_tab[1]))
-	{
-		ft_printf("\n");
-		return ;
+		i = 1;
+		if (!command_tab[1] || is_only_space(command_tab[1]))
+		{
+			ft_printf("\n");
+			return ;
+		}
+		if (command_tab[i] && !ft_strncmp(command_tab[i], "-n", 3))
+		{
+			while (command_tab[i] && !ft_strncmp(command_tab[i], "-n", 3))
+				i++;
+		}
+		j = i;
+		while (command_tab[i])
+		{
+			ft_printf("%s", command_tab[i++]);
+			if (command_tab[i] && command_tab[i][0])
+				ft_printf(" ");
+		}
+		if (j == 1)
+			ft_printf("\n");
+		if (data)
+			data->exit_status = 0;
 	}
-	if (!ft_strncmp(command_tab[i], "-n", 3))
-	{
-		while (!ft_strncmp(command_tab[i], "-n", 3))
-			i++;
-	}
-	j = i;
-	while (command_tab[i])
-	{
-		ft_printf("%s", command_tab[i++]);
-		if (command_tab[i] && command_tab[i][0])
-			ft_printf(" ");
-	}
-	if (j == 1)
-		ft_printf("\n");
-	if (data)
-		data->exit_status = 0;
-}
 
 /**========================================================================
  *                           is_only_space
