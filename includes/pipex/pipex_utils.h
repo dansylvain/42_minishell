b9c4358eb49edc9ce5863ea71bcbc2f65deb5fc5
sierra182 <sierra-echo@msn.com>
2024-03-08 18:52:41 +0100

    continue norm headers

--- includes/pipex/pipex_utils.h
+++ includes/pipex/pipex_utils.h
@@ -6,22 +6,22 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 18:01:34 by seblin            #+#    #+#             */
-/*   Updated: 2024/03/08 18:28:09 by seblin           ###   ########.fr       */
+/*   Updated: 2024/03/08 18:51:00 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_UTILS_H
 # define PIPEX_UTILS_H
 
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include "pipex_global_utils.h"
-#include "ft_printf.h"
-#include "../../lib/gnl/get_next_line.h"
-#include "libft.h"
-#include "parsing_utils.h"
-#include "minishell.h"
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include "pipex_global_utils.h"
+# include "ft_printf.h"
+# include "../../lib/gnl/get_next_line.h"
+# include "libft.h"
+# include "parsing_utils.h"
+# include "minishell.h"
 
 void	display_error(char *str);
 void	store_and_free_cmd_list(t_ast_nde *cmd_list);
