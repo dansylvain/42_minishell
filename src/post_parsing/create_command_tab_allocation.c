2b2e38828d9226fa5426c560eadeb3d36965377a
dansylvain <dansylvain@gmail.com>
2024-03-13 09:02:41 +0100

    dan files norminated

--- src/post_parsing/create_command_tab_allocation.c
+++ src/post_parsing/create_command_tab_allocation.c
@@ -6,25 +6,11 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 13:43:56 by dan               #+#    #+#             */
-/*   Updated: 2024/03/13 07:47:31 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/13 08:58:25 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parsing_utils.h"
-#include "minishell.h"
-#include "libft.h"
-#include "ft_printf.h"
-
-int		is_separator(t_ast_nde *node);
-int		is_chevron(t_ast_nde *node);
-void	display_command_tab_big(char ***command_tab);
-void	print_node(t_ast_nde *node);
-void	print_tree(t_ast_nde *node);
-char	*get_node_str(t_ast_nde *node);
-int		get_pipe_parts_nbr(t_ast_nde **node);
-char	***alloc_memory_for_pipe_parts(char ***cmd_tab, int pipe_parts_nbr);
-int		get_pipe_elements_nbr(t_ast_nde *node);
-char	**alloc_memory_for_pipe_elements(char **cmd_tab, int pipe_elements_nbr);
+#include "create_command_tab_allocation.h"
 
 /**========================================================================
  *                           get_pipe_parts_nbr
