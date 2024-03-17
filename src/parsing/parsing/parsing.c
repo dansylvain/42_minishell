257859242ac420824017d7ca042030c9b1d2bc8d
sierra182 <sierra-echo@msn.com>
2024-03-17 15:57:36 +0100

    del is_double

--- src/parsing/parsing/parsing.c
+++ src/parsing/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 15:18:58 by seblin            #+#    #+#             */
-/*   Updated: 2024/03/17 14:08:49 by seblin           ###   ########.fr       */
+/*   Updated: 2024/03/17 15:55:39 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,27 @@ extern int	m_flag;//!! remetre a un ds prompt loop
 extern int	p_flag;
 extern int	r_flag;
 
-static int	is_double_token(t_ast_nde *node)
-{
-	t_tok	d_tok;
+// static int	is_double_token(t_ast_nde *node)
+// {
+// 	t_tok	d_tok;
 
-	d_tok = RAW;
-	while (node)
-	{
-		if (node->token != RAW && !is_chevron(node) && node->token != JOKER && node->token != DOLL)
-		{
-			// if (d_tok < 0)
-// 				return (display_error_free(ft_strjoin("minishell: syntax 
-// error near unexpected token DTOK 0 ", translate_enum(d_tok))), 1); //!pas bonn ne marche pas si token diff
-			if (node->token == d_tok)
-				return (display_error_free(ft_strjoin("minishell: syntax \
-error near DOUBLEunexpected token ", translate_enum(d_tok))), 1);
-		}
-		d_tok = node->token;
-		node = node->sibling;
-	}
-	return (0);
-}
+// 	d_tok = RAW;
+// 	while (node)
+// 	{
+// 		if (node->token != RAW && !is_chevron(node) && node->token != JOKER && node->token != DOLL)
+// 		{
+// 			// if (d_tok < 0)
+// // 				return (display_error_free(ft_strjoin("minishell: syntax 
+// // error near unexpected token DTOK 0 ", translate_enum(d_tok))), 1); //!pas bonn ne marche pas si token diff
+// 			if (node->token == d_tok)
+// 				return (display_error_free(ft_strjoin("minishell: syntax \
+// error near DOUBLEunexpected token ", translate_enum(d_tok))), 1);
+// 		}
+// 		d_tok = node->token;
+// 		node = node->sibling;
+// 	}
+// 	return (0);
+// }
 
 static void	set_root(t_ast_nde **root, char *str)
 {
@@ -59,13 +59,7 @@ int	parse_par(char *str, t_Data *data, t_ast_nde *root)
 	t_ast_nde	*cmd;
 	t_ast_nde	*quote;
 	int			first_rec;
-	// if (root)
-	// {
-	// 	ft_printf("parse par\n");
-	// 	print_node(root);
-	// 	ft_printf("\n");
-	// }
-	//m_flag = 0;
+
 	r_flag = 0;
 	first_rec = 0;
 	if (!*str)
@@ -119,7 +113,7 @@ t_ast_nde	*parse(char *str, t_Data *data)
 	if (set_operator(root->child))
 		return (free_tree(root), NULL);	
 	leaf_tree(root, &cmd, &cmd_sav, data);
-	if (is_double_token(cmd_sav))
-		return (free_sibling_and_child(cmd_sav), free_tree(root), NULL);
+	// if (is_double_token(cmd_sav))
+	// 	return (free_sibling_and_child(cmd_sav), free_tree(root), NULL);
 	return (free_tree(root), cmd_sav);
 }
