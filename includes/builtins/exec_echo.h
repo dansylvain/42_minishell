eaecbeb297d576b3594a758c264090440d609921
dansylvain <dansylvain@gmail.com>
2024-03-18 05:30:07 +0100

    norminated exec_echo

--- includes/builtins/exec_echo.h
+++ includes/builtins/exec_echo.h
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 18:52:53 by dan               #+#    #+#             */
-/*   Updated: 2024/03/08 19:42:28 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/18 05:29:23 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@
 void	exec_echo(t_Data *data, char **command_tab);
 int		is_only_space(char *str);
 int		is_not_empty_prompt(char *str);
+t_Data	*get_data(char *envp[]);
 
 #endif
